de55b237a6f18300aa5eaf20ac8e6c79a04fb107
Rhamseys Garcia <rhamseysgarcia@MacBook-Pro.local>
2024-04-18 16:57:52 -0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import * as pages from './App.js';
+//import StarsCanvas from './Components/canvas/Stars';
 
 function App() {
 
   return (
     <>
-      <p>App</p>
+      <pages.ContactForm />
+
     </>
   )
 }
